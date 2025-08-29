f8c3911260b747979512fda1dcd0030017b74756
Ahmed Behairy <ahmed.behairy123@gmail.com>
2025-08-29 17:31:20 +0300

    Q11

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -205,7 +205,18 @@ namespace Assignment
 
             #endregion
 
-            
+            #region Q11
+            //11.Find the union of two sets, returning the unique elements from both sets
+
+            //HashSet<int> set1 = new HashSet<int> { 1, 2, 3, 4, 5 };
+            //HashSet<int> set2 = new HashSet<int> { 3, 4, 5, 6, 7 };
+
+            //set1.UnionWith(set2); 
+            //Console.WriteLine(string.Join(", ", set1));
+
+
+            #endregion
+
 
         }
     }
