c543549bd0d043be89eda75af5ae7667aff888a2
Ahmed Behairy <ahmed.behairy123@gmail.com>
2025-08-29 17:26:37 +0300

    Q01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -16,7 +16,31 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
-            
+            #region Q01
+            //1.Given an array of integers, count the frequency of each element using a hash table.
+
+            //Hashtable ht = new Hashtable();
+
+            //int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1 };
+
+            //foreach (int num in arr)
+            //{
+            //    if (ht.ContainsKey(num))
+            //        ht[num] = (int)ht[num] + 1;
+
+            //    else     
+            //        ht[num] = 1;
+
+            //}
+
+            //foreach (DictionaryEntry entry in ht)
+            //{
+            //    Console.WriteLine($"Element: {entry.Key}, Frequency: {entry.Value}");
+
+
+            #endregion
+
+
 
         }
     }
