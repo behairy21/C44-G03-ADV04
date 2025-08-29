0884d6aa896dc9959209c5780585eeb4ccb30dfd
Ahmed Behairy <ahmed.behairy123@gmail.com>
2025-08-29 17:25:17 +0300

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.IO;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using System.Xml;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+
+}
